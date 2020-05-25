67ec2ed78aa1737f02f74d5c6bc71e563ba43396
Виктор Кочетков <victor.development@yandex.ru>
2020-05-25 18:16:47 +0300

    Update WeatherMT.cs

--- RockFramework/Iridium360/Connect/Framework/Messaging/WeatherMT.cs
+++ RockFramework/Iridium360/Connect/Framework/Messaging/WeatherMT.cs
@@ -30,32 +30,32 @@ namespace Iridium360.Connect.Framework.Messaging
         /// <summary>
         /// Облачность в процентах (%)
         /// </summary>
-        public int? _cloud { get; set; }
+        public int? Cloud { get; set; }
 
 
         /// <summary>
         /// 
         /// </summary>
-        public int? Cloud
-        {
-            get
-            {
-                if (_cloud == null)
-                    return null;
+        //public int? Cloud
+        //{
+        //    get
+        //    {
+        //        if (_cloud == null)
+        //            return null;
 
-                return (int)Math.Round(_cloud.Value * 20d);
-            }
-            set
-            {
-                if (value == null)
-                {
-                    _cloud = null;
-                    return;
-                }
+        //        return (int)Math.Round(_cloud.Value * 20d);
+        //    }
+        //    set
+        //    {
+        //        if (value == null)
+        //        {
+        //            _cloud = null;
+        //            return;
+        //        }
 
-                _cloud = (int)Math.Round(value.Value / 20d);
-            }
-        }
+        //        _cloud = (int)Math.Round(value.Value / 20d);
+        //    }
+        //}
 
         /// <summary>
         /// Осадки в мм
@@ -203,8 +203,8 @@ namespace Iridium360.Connect.Framework.Messaging
 
             foreach (var f in Forecasts)
             {
-                biterator.PushFloat((float)f.Lat, true, 5);
-                biterator.PushFloat((float)f.Lon, true, 5);
+                biterator.PushFloat((float)f.Lat, true, 10);
+                biterator.PushFloat((float)f.Lon, true, 10);
 
                 biterator.PushInt(f.TimeOffset,
                     GetBits(min: -12, max: 14));
@@ -228,8 +228,13 @@ namespace Iridium360.Connect.Framework.Messaging
                         biterator.PushInt(ff.Temperature,
                             GetBits(min: -70, max: 50));
 
-                        biterator.PushUInt((uint)(ff._cloud ?? 21),
-                            GetBits(min: 0, max: 21));
+                        int? _cloud = ff.Cloud;
+
+                        if (_cloud != null)
+                            _cloud = (int)Math.Round(_cloud.Value / 20d);
+
+                        biterator.PushUInt((uint)(_cloud ?? 21),
+                            GetBits(min: 0, max: 20 + 1));
 
                         double? _precipitation = ff.Precipitation;
 
@@ -239,7 +244,7 @@ namespace Iridium360.Connect.Framework.Messaging
                             _precipitation = (int)Math.Round(_precipitation.Value * 4d);
 
                         biterator.PushUInt((uint)(_precipitation ?? 241),
-                            GetBits(min: 0, max: 241));
+                            GetBits(min: 0, max: 240 + 1));
 
                         int? _windDirection = ff.WindDirection;
 
@@ -247,7 +252,7 @@ namespace Iridium360.Connect.Framework.Messaging
                             _windDirection = (int)(_windDirection.Value / 45d);
 
                         biterator.PushUInt((uint)(_windDirection ?? 9),
-                            GetBits(min: 0, max: 9));
+                            GetBits(min: 0, max: 8 + 1));
 
                         double? _windSpeed = ff.WindSpeed;
 
@@ -255,7 +260,7 @@ namespace Iridium360.Connect.Framework.Messaging
                             _windSpeed = (int)Math.Round(_windSpeed.Value * 2d);
 
                         biterator.PushUInt((uint)(_windSpeed ?? 61),
-                            GetBits(min: 0, max: 61));
+                            GetBits(min: 0, max: 60 + 1));
 
                         double? _pressure = ff.Pressure;
 
@@ -263,10 +268,10 @@ namespace Iridium360.Connect.Framework.Messaging
                             _pressure -= 580;
 
                         biterator.PushUInt((uint)(_pressure ?? 222),
-                            GetBits(min: 0, max: 222));
+                            GetBits(min: 0, max: 221 + 1));
 
                         biterator.PushUInt((uint)(ff.SnowRisk != null ? (ff.SnowRisk.Value ? 1 : 0) : 2),
-                            GetBits(min: 0, max: 2));
+                            GetBits(min: 0, max: 1 + 1));
                     }
 
                 }
@@ -292,8 +297,8 @@ namespace Iridium360.Connect.Framework.Messaging
             {
                 var f = new WeatherMT_PointForecast();
 
-                f.Lat = biterator.PopFloat(true, 5);
-                f.Lon = biterator.PopFloat(true, 5);
+                f.Lat = biterator.PopFloat(true, 10);
+                f.Lon = biterator.PopFloat(true, 10);
 
                 f.TimeOffset = biterator.PopInt(GetBits(min: -12, max: 14));
 
@@ -317,12 +322,14 @@ namespace Iridium360.Connect.Framework.Messaging
 
                         ff.Temperature = biterator.PopInt(GetBits(min: -70, max: 50));
 
-                        ff._cloud = (int)biterator.PopUInt(GetBits(min: 0, max: 21));
+                        int _cloud = (int)biterator.PopUInt(GetBits(min: 0, max: 20 + 1));
 
-                        if (ff._cloud == 21)
-                            ff._cloud = null;
+                        if (_cloud == 21)
+                            ff.Cloud = null;
+                        else
+                            ff.Cloud = _cloud * 20;
 
-                        int _precipitation = (int)biterator.PopUInt(GetBits(min: 0, max: 241));
+                        int _precipitation = (int)biterator.PopUInt(GetBits(min: 0, max: 240 + 1));
 
                         if (_precipitation == 241)
                             ff.Precipitation = null;
@@ -330,28 +337,28 @@ namespace Iridium360.Connect.Framework.Messaging
                             ff.Precipitation = _precipitation / 4d;
 
 
-                        int _windDirection = (int)biterator.PopUInt(GetBits(min: 0, max: 9));
+                        int _windDirection = (int)biterator.PopUInt(GetBits(min: 0, max: 8 + 1));
 
                         if (_windDirection == 9)
                             ff.WindDirection = null;
                         else
                             ff.WindDirection = (int)Math.Round(_windDirection * 45d);
 
-                        int _windSpeed = (int)biterator.PopUInt(GetBits(min: 0, max: 61));
+                        int _windSpeed = (int)biterator.PopUInt(GetBits(min: 0, max: 60 + 1));
 
                         if (_windSpeed == 61)
                             ff.WindSpeed = null;
                         else
                             ff.WindSpeed = _windSpeed / 2d;
 
-                        int _pressure = (int)biterator.PopUInt(GetBits(min: 0, max: 222));
+                        int _pressure = (int)biterator.PopUInt(GetBits(min: 0, max: 221 + 1));
 
                         if (_pressure == 222)
                             ff.Pressure = null;
                         else
                             ff.Pressure = _pressure + 580;
 
-                        int? _snowRisk = (int)biterator.PopUInt(GetBits(min: 0, max: 2));
+                        int? _snowRisk = (int)biterator.PopUInt(GetBits(min: 0, max: 1 + 1));
 
                         if (_snowRisk == 2)
                             ff.SnowRisk = null;
